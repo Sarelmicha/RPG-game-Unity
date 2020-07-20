e7ead83845f1f4a46dc1e37b29436f125c547b13
Sarel <sarelmicha93@gmail.com>
2020-07-20 12:58:46 +0300

    *Fix navMesh - path is not to long and cursor show only valid places to go

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -7,6 +7,7 @@ using RPG.Resources;
 using UnityEngine.EventSystems;
 
 using System;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -22,6 +23,8 @@ namespace RPG.Control
         }
 
         [SerializeField] CursorMapping[] cursorMappings = null;
+        [SerializeField] float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] float maxNavPathLength = 40f;
 
         private void Awake()
         {
@@ -99,20 +102,82 @@ namespace RPG.Control
 
         private bool InteractWithMovement()
         {
-            
-            RaycastHit hit;
+
+            //RaycastHit hit;
             //Check if the ray has hit somthing
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            //bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            Vector3 target;
+            bool hasHit = RaycastNavMesh(out target);
             if (hasHit)
             {
                 if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Mover>().StartMoveAction(hit.point,1f);
+                    GetComponent<Mover>().StartMoveAction(target,1f);
                 }
               
                 return true;
             }
             return false;
+        }
+
+        private bool RaycastNavMesh(out Vector3 target)
+        {
+            target = new Vector3();
+            //Raycast to terrain
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+
+            if (!hasHit)
+            {
+                return false;
+            }
+
+            //Find nearest navmesh point
+           NavMeshHit navMeshHit;
+           bool hasCastToNavMesh =  NavMesh.SamplePosition(hit.point, out navMeshHit, maxNavMeshProjectionDistance, NavMesh.AllAreas);
+
+            if (!hasCastToNavMesh)
+            {
+                return false;
+            }
+
+            target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath)
+            {
+                return false;
+            }
+            if (path.status != NavMeshPathStatus.PathComplete)
+            {
+                return false;
+            }
+
+            if (GetPathLength(path) > maxNavPathLength)
+            {
+                return false;
+            }
+
+            return true;
+        
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+
+            if (path.corners.Length < 2)
+            {
+                return total;
+            }
+
+            for (int i = 0; i < path.corners.Length - 1 ; i++)
+            {
+                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+
+            return total;
         }
 
         private void SetCursor(CursorType type)
